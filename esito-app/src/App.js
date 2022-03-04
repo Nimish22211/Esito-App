3f924d419c5a453fae97c36fb79ff8c099a271e5
Nimish <shailbandha@gmail.com>
2022-03-04 23:25:18 +0530

    header

--- esito-app/src/App.js
+++ esito-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Homepage />
     </div>
   );
 }
---
+++ esito-app/src/Components/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Homepage.css'
+
+function Homepage() {
+    return (
+        <div>
+            <header>
+                <span>Esito For Devs</span>
+                <div>
+                    <ul>
+                        <li>ABCD</li>
+                        <li>Team</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Plans</li>
+                        <li>Bugs & Errors</li>
+                        <li className="loginbtn">Login</li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Homepage
