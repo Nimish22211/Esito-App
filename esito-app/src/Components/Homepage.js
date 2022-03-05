dc9a98879393aafce491a3105d7c840987452275
Nimish <shailbandha@gmail.com>
2022-03-06 00:44:12 +0530

    get started part

--- esito-app/src/Components/Homepage.js
+++ esito-app/src/Components/Homepage.js
@@ -5,10 +5,10 @@ function Homepage() {
     return (
         <div>
             <header>
-                <span>Esito For Devs</span>
+                <span className="logo">Esito For Devs</span>
                 <div>
                     <ul>
-                        <li>ABCD</li>
+                        <li>Home</li>
                         <li>Team</li>
                         <li>About</li>
                         <li>Contact</li>
@@ -18,6 +18,27 @@ function Homepage() {
                     </ul>
                 </div>
             </header>
+            <main>
+                <div className="get-started">
+                    <div className="card">
+                        <h2>Fixing Lot's of Bugs & Errors ?</h2>
+                        <h3>Let's Get Started</h3>
+                        <p>
+                            If You are a Developer You Face Lot's of Bugs and Errors
+                            . That's Why We Build this Platform For YouYou can Click on
+                            Get Started Button to Get Startedwith the Platform and You c
+                            an Post Your Bugs or Errors Here and also You can Help Some
+                            one Whois Struggling to Fix Bugs and Errors .
+                        </p>
+                        <button>Get Started</button>
+                    </div>
+                    <img src='Images/struggle illus.png' />
+                </div>
+                <h2>Latest Bugs & Errors</h2>
+                <div className="card">
+
+                </div>
+            </main>
         </div>
     )
 }
